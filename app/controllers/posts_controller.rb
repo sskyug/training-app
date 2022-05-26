4bed5c76e6c40cf041206447547636c37837d608
sskyug <sskyugo972@gmail.com>
2022-05-26 10:44:53 +0900

    postコントローラー作成とマイグレーション実行

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+  before_action :to_root, except: [:index]
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to root_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:training_name, :detail).merge(user_id: current_user.id)
+  end
+  def to_root
+    redirect_to root_path unless user_signed_in?
+  end
+end
---
+++ db/migrate/20220520101059_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :training_name, null:false
+      t.text :detail, null:false
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
